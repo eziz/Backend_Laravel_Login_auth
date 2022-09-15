9b38da6a34b1eeaba589c1ce433a1dcf4eba1bc7
Eziz Soyun <ezizce@gmail.com>
2022-09-15 15:54:47 +0500

    4th version

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -4,8 +4,52 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Student;
+use Illuminate\Support\Facades\Hash;
 
 class StudentController extends Controller
 {
-    //
+    //REGISTER API
+    public function register(Request $request) // REQUEST INSTANCE
+    {
+        //validation
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:students",
+            "password" => "required|confirmed"
+        ]);
+
+        //create data
+        $student = new Student();
+
+        $student->name = $request->name;
+        $student->email = $request->email;
+        $student->password = Hash::make($request->password);
+        $student->phone_no = isset($request->phone_no) ? $request->phone_no : "";
+
+        $student->save();
+
+
+        //send response
+
+        return response()->json([
+            "status" => 1,
+            "message" => "Student registered successfully"
+        ]);
+    }
+
+    //LOGIN API
+    public function login(Request $request) // REQUEST INSTANCE
+    {
+    }
+
+    //PROFILE API
+    public function profile()
+    {
+    }
+
+    //LOGOUT API
+    public function logout()
+    {
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\Api\ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,13 +18,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('register', [\App\Http\Controllers\StudentController::class, 'register']);
+Route::post('login', [\App\Http\Controllers\StudentController::class, 'login']);
 
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::get("profile", [StudentController::class, "profile"]);
+    Route::get("logout", [StudentController::class, "logout"]);
 
-Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
-Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
-
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
-    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post("create-project", [ProjectController::class, "createProject"]);
+    Route::get("list-project", [ProjectController::class, "listProject"]);
+    Route::get("single-project/{id}", [ProjectController::class, "singleProject"]);
+    Route::delete("delete-project/{id}", [ProjectController::class, "deleteProject"]);
 });
+
+
+
+//Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+//Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+
+//Route::middleware('auth:sanctum')->group(function () {
+ //   Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
+ //   Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+//});
